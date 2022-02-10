e07a9eda0bc7bb811dc1fe4e16520b59a05c5f10
Pacharawut Thanawut <tpacharawut@hotmail.com>
2022-02-10 14:16:48 +0700

    change path to modules

--- find_monitor.py
+++
@@ -1,7 +0,0 @@
-import pyautogui
-
-class finding_monitor:
-    def find_monitor():
-        monitor = pyautogui.getWindowsWithTitle("Farmer")
-        monitor_leng = len(monitor)
-        return monitor, monitor_leng
